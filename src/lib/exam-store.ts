79f92a1cba1c3ce4fe9c3b74d41bf30908574eb5
Deepak Kumar <dk3624897@gmail.com>
2026-01-15 06:20:07 +0000

    jitna bhi student result list hai faculty dashboard me class 6 to 9th sa

--- src/lib/exam-store.ts
+++ src/lib/exam-store.ts
@@ -155,7 +155,8 @@ const DEFAULT_RESULTS: { [studentId: string]: { [examId: string]: ExamResult } }
     '04-9-Daffodils': { '4': { robotics: 21, coding: 0 } },
     '05-9-Daffodils': { '4': { robotics: 40, coding: 0 } },
     '07-9-Daffodils': { '4': { robotics: 3, coding: 0 } },
-    '09-9-Daffodils': { '4': { robotics: 19, coding: 0 } },
+    '09-9-Daffodils': { '4': { robotics: 29, coding: 0 } }, // aryan joshi
+    '09-9-Daisies':   { '5': { robotics: 0, coding: 0 } }, // bilal.khan - Note: Roll No. conflict, placed in Daisies as per file
     '10-9-Daffodils': { '4': { robotics: 21, coding: 0 } },
     '11-9-Daffodils': { '4': { robotics: 0, coding: 0 } },
     '12-9-Daffodils': { '4': { robotics: 0, coding: 0 } },
@@ -180,28 +181,27 @@ const DEFAULT_RESULTS: { [studentId: string]: { [examId: string]: ExamResult } }
     '36-9-Daffodils': { '4': { robotics: 5, coding: 0 } },
     '37-9-Daffodils': { '4': { robotics: 0, coding: 0 } },
     '38-9-Daffodils': { '4': { robotics: 0, coding: 0 } },
-    '39-9-Daffodils': { '4': { robotics: 35, coding: 0 } },
+    '39-9-Daffodils': { '4': { robotics: 13, coding: 0 } },
     '40-9-Daffodils': { '4': { robotics: 11, coding: 0 } },
     
     // Class 9 Daisies
-    '02-9-Daisies': { '5': { robotics: 27, coding: 0 } }, // AASHVIK KADAM - mcqScore 27 (mcqCorrect 10)
+    '02-9-Daisies': { '5': { robotics: 27, coding: 0 } },
     '03-9-Daisies': { '5': { robotics: 3, coding: 0 } },
     '05-9-Daisies': { '5': { robotics: 37, coding: 0 } },
-    '06-9-Daisies': { '5': { robotics: 16, coding: 0 } }, // arnav patil - mcqScore 16 (mcqCorrect 6)
+    '06-9-Daisies': { '5': { robotics: 16, coding: 0 } },
     '08-9-Daisies': { '5': { robotics: 24, coding: 0 } },
-    '09-9-Daisies': { '5': { robotics: 0, coding: 0 } },
     '10-9-Daisies': { '5': { robotics: 0, coding: 0 } },
     '13-9-Daisies': { '5': { robotics: 0, coding: 0 } },
     '14-9-Daisies': { '5': { robotics: 29, coding: 0 } },
-    '15-9-Daisies': { '5': { robotics: 29, coding: 0 } }, // Janhavi Avinash Bhoir - mcqScore 29 (mcqCorrect 11)
+    '15-9-Daisies': { '5': { robotics: 29, coding: 0 } },
     '16-9-Daisies': { '5': { robotics: 0, coding: 0 } },
-    '19-9-Daisies': { '5': { robotics: 29, coding: 0 } }, // Ketki Jitendra Talele - mcqScore 29 (mcqCorrect 11)
+    '19-9-Daisies': { '5': { robotics: 29, coding: 0 } },
     '20-9-Daisies': { '5': { robotics: 16, coding: 0 } },
-    '21-9-Daisies': { '5': { robotics: 9, coding: 0 } }, // NISHANT BRAMHARAKSHAS - mcqScore 9 (mcqCorrect 3)
+    '21-9-Daisies': { '5': { robotics: 9, coding: 0 } },
     '23-9-Daisies': { '5': { robotics: 37, coding: 0 } },
-    '26-9-Daisies': { '5': { robotics: 32, coding: 0 } }, // SOHAM MARBHAL - mcqScore 32 (mcqCorrect 12)
+    '26-9-Daisies': { '5': { robotics: 32, coding: 0 } },
     '27-9-Daisies': { '5': { robotics: 27, coding: 0 } },
-    '28-9-Daisies': { '5': { robotics: 9, coding: 0 } }, // SUYASH.RAUL - mcqScore 9 (mcqCorrect 3)
+    '28-9-Daisies': { '5': { robotics: 9, coding: 0 } },
     '30-9-Daisies': { '5': { robotics: 0, coding: 0 } },
     '33-9-Daisies': { '5': { robotics: 24, coding: 0 } },
     '35-9-Daisies': { '5': { robotics: 0, coding: 0 } },
